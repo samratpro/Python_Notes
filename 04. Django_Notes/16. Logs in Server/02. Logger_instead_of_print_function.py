917d90311522885383f420b6d9311b4287b9ee88
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2024-12-27 21:12:41 +0600

    Update 02. Logger_instead_of_print_function.py

--- 04. Django_Notes/16. Logs in Server/02. Logger_instead_of_print_function.py
+++ 04. Django_Notes/16. Logs in Server/02. Logger_instead_of_print_function.py
@@ -1,12 +1,13 @@
 # Using logger instead of print function, cause live server can't show data like VS Code or Pycharm terminal
-
 import logging
+from datetime import datetime
 # Get an instance of a logger
 logger = logging.getLogger("django")
 
 def your_view_function(request):
     name = 'Samrat'
     age = 25
+    logger.info(f'{datetime.now()} - Message...')
     logger.info('Appname.filename.py >>> This is bulk info posting page, testing for Python logger')
     logger.info(f'Appname.filename.py >>> This message in from logger, name: {name} and  age :{str(age)}')
     # Logger can take one argument not like print() function
