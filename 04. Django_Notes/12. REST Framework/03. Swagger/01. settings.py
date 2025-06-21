f0dca629ca067f75f4a74dbee651c28ac64ab7f7
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2025-06-21 18:24:11 +0600

    Update 01. settings.py

--- 04. Django_Notes/12. REST Framework/03. Swagger/01. settings.py
+++ 04. Django_Notes/12. REST Framework/03. Swagger/01. settings.py
@@ -6,3 +6,14 @@ INSTALLED_APPS = [
     'drf_spectacular',
 ]
 
+
+REST_FRAMEWORK = {
+         'DEFAULT_AUTHENTICATION_CLASSES': [
+             'rest_framework.authentication.TokenAuthentication',
+             'rest_framework.authentication.SessionAuthentication',
+         ],
+         'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+        'DEFAULT_PERMISSION_CLASSES': [
+                'rest_framework.permissions.AllowAny',
+            ],
+     }
