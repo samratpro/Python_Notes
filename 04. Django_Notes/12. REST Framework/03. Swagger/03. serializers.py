cbd84c0571be6f38c5420e4292adeca69951a3d7
Samrat Biswas <102436136+samratpro@users.noreply.github.com>
2025-06-21 18:31:50 +0600

    Create 03. serializers.py

---
+++ 04. Django_Notes/12. REST Framework/03. Swagger/03. serializers.py
@@ -0,0 +1,48 @@
+from rest_framework import serializers
+from .models import Software
+
+class SoftwareSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Software
+        fields = '__all__'
+
+
+class DeviceRegistrationSerializer(serializers.Serializer):
+    email = serializers.CharField(
+        max_length=254,
+        required=True,
+        allow_blank=False,
+        allow_null=False,
+        label='Email',
+        help_text='User email address'
+    )
+    password = serializers.CharField(
+        required=True,
+        write_only=True,
+        allow_blank=False,
+        allow_null=False,
+        label='Password',
+        help_text='User password',
+        style={'input_type': 'password'}
+    )
+    software_id = serializers.IntegerField(
+        required=True,
+        label='Software ID',
+        help_text='ID of the software',
+        min_value=1
+    )
+    device_info = serializers.DictField(
+        required=True,
+        allow_null=False,
+        help_text='Device information (e.g., mac_address, device_name, device_type, os_version, ip_address, location)'
+    )
+
+    def validate_device_info(self, value):
+        if not isinstance(value, dict):
+            raise serializers.ValidationError("device_info must be a dictionary")
+        if 'mac_address' not in value or not value['mac_address']:
+            raise serializers.ValidationError("device_info must include a non-empty mac_address")
+        return value
+
+    def validate(self, data):
+        return data
